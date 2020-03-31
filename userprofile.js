fc135fdcaf538474781d80ccf3c1846e438715a5
dobredani <dobre.dani.gtalk@gmail.com>
2020-03-31 12:39:06 +0300

    collapsible

--- userprofile.js
+++ userprofile.js
@@ -10,3 +10,21 @@ function showSomething(str) {
     document.getElementById("customli").innerHTML = str;
     return str;
 }
+
+function initCollapsible() {
+    var coll = document.getElementsByClassName("collapsible-container");
+    var i;
+
+    for (i = 0; i < coll.length; i++){
+        coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+}
+}
