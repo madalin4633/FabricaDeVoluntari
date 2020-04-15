9250627e72002a329ad4175e80acb539feb8a4e0
madalin4633 <costel_ark@yahoo.com>
2020-04-15 12:15:53 +0300

    Update echo.php

--- echo.php
+++ echo.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <?php header("Access-Control-Allow-Origin: https://afternoon-bayou-37223.herokuapp.com/"); ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
