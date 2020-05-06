8b08c36ea85d7719340b52039a14fbca746e072f
dobredani <dobre.dani.gtalk@gmail.com>
2020-05-06 23:15:21 +0300

    detailed db api

--- app/api/createTables.api.php
+++ app/api/createTables.api.php
@@ -1,5 +1,5 @@
 <?php
-/* 
+/*
 
 Create tables in the database
 
@@ -11,26 +11,69 @@ SQL Syntax: postgreSQL
 
 header("Content-type: application/json");
 
-class createTablesAPI extends GenericAPI {
+class createTablesAPI extends GenericAPI
+{
+    public function __construct()
+    {
+        require_once __DIR__ . "/../models/genericModel.php";
 
-    public function __construct() {
         switch ($_SERVER['REQUEST_METHOD']) {
-            case "GET":
-                if (preg_match('/api\/createTables\/(([0-9,a-z,A-Z,-])+)(\/)?$/', $_SERVER['REQUEST_URI'], $matches)) {
-                    echo "{response: 'API GET'}";
+            case "DELETE":
+                // api/createTables/Associations
+                if (preg_match('/api\/createTables\/Associations(\/)?$/i', $_SERVER['REQUEST_URI'], $matches)) {
+                    $this -> middlewares([['dropTableAssociations', [$GLOBALS['db']]]]);
+                
+                // api/createTables/Volunteers
+                } elseif (preg_match('/api\/createTables\/Volunteers(\/)?$/i', $_SERVER['REQUEST_URI'], $matches)) {
+                    $this -> middlewares([['dropTableVolunteers', [$GLOBALS['db']]]]);
+                        
+                // api/createTables/VolAssoc
+                } elseif (preg_match('/api\/createTables\/VolAssoc(\/)?$/i', $_SERVER['REQUEST_URI'], $matches)) {
+                    $this -> middlewares([['dropTableVolAssoc', [$GLOBALS['db']]]]);
+
                 } else {
                     http_response_code(400);
                     echo '{"status":400, "response":"Bad request!"}';
                 }
                 break;
-            case "POST":
-                if (preg_match('/api\/createTables(\/)?$/', $_SERVER['REQUEST_URI'], $matches)) {
-                    $this -> validateCreateTables();
+                case "PUT":
+                    // api/createTables/Associations
+                    if (preg_match('/api\/createTables\/Associations(\/)?$/i', $_SERVER['REQUEST_URI'], $matches)) {
+                        $this -> middlewares([['insertDataAssociations', [$GLOBALS['db']]]]);
                     
+                    // api/createTables/Volunteers
+                    } elseif (preg_match('/api\/createTables\/Volunteers(\/)?$/i', $_SERVER['REQUEST_URI'], $matches)) {
+                        $this -> middlewares([['insertDataVolunteers', [$GLOBALS['db']]]]);
+                            
+                    // api/createTables/VolAssoc
+                    } elseif (preg_match('/api\/createTables\/VolAssoc(\/)?$/i', $_SERVER['REQUEST_URI'], $matches)) {
+                        $this -> middlewares([['insertDataVolAssoc', [$GLOBALS['db']]]]);
+    
+                    } else {
+                        http_response_code(400);
+                        echo '{"status":400, "response":"Bad request!"}';
+                    }
+                    break;
+                case "POST":
+                // api/createTables/Associations
+                if (preg_match('/api\/createTables\/Associations(\/)?$/i', $_SERVER['REQUEST_URI'], $matches)) {
+                    $this -> middlewares([['createTableAssociations', [$GLOBALS['db']]]]);
+                
+                // api/createTables/Volunteers
+                } elseif (preg_match('/api\/createTables\/Volunteers(\/)?$/i', $_SERVER['REQUEST_URI'], $matches)) {
+                    $this -> middlewares([['createTableVolunteers', [$GLOBALS['db']]]]);
+                        
+                // api/createTables/VolAssoc
+                } elseif (preg_match('/api\/createTables\/VolAssoc(\/)?$/i', $_SERVER['REQUEST_URI'], $matches)) {
+                    $this -> middlewares([['createTableVolAssoc', [$GLOBALS['db']]]]);
+                        
+                // api/createTables
+                } elseif (preg_match('/api\/createTables(\/)?$/i', $_SERVER['REQUEST_URI'], $matches)) {
+                    $this -> middlewares([['createTables', [$GLOBALS['db']]]]);
+
                 } else {
                     http_response_code(400);
                     echo '{"status":400, "response":"Bad request!"}';
-                    
                 }
                 break;
             default:
@@ -40,7 +83,8 @@ class createTablesAPI extends GenericAPI {
         }
     }
 
-    private function validateCreateTables() {
+    private function validateCreateTables()
+    {
         // if user is authentificated (admin), then create tables in the database
         
                 
@@ -51,8 +95,10 @@ class createTablesAPI extends GenericAPI {
 }
 
 
-class GenericAPI {
-    protected function middlewares($array) {
+class GenericAPI
+{
+    protected function middlewares($array)
+    {
         for ($i = 0; $i < sizeof($array); $i++) {
             echo $array[$i][0];
             if (! $array[$i][0]($array[$i][1][0])) {
@@ -60,13 +106,15 @@ class GenericAPI {
             }
         }
     }
-    protected function adminOnly() {
+    protected function adminOnly()
+    {
         //TODO check if user is admin
 
         http_response_code(400);
         return true;
     }
 
-    protected function volunteerOnly() {}
-
+    protected function volunteerOnly()
+    {
+    }
 }
--- app/models/genericModel.php
+++ app/models/genericModel.php
@@ -1,16 +1,78 @@
 <?php
+/**
+ * Model for api calls:
+ * POST api/createTables/ => createTables()
+ * POST api/createTables/Associations => createTableAssoc()
+ * POST api/createTables/Volunteers => createTableVolunteers()
+ * POST api/createTables/VolAssoc => createTableVolAssoc()
+ *
+ * 
+ * DELETE api/createTables/Associations => dropTableAssoc()
+ * DELETE api/createTables/Volunteers => dropTableVolunteers()
+ * DELETE api/createTables/VolAssoc => dropTableVolAssoc()
+ *
+ * PUT api/createTables/Associations => insertDataAssociations()
+ * PUT api/createTables/Volunteers => insertDataVolunteers()
+ * PUT api/createTables/VolAssoc => insertDataVolAssoc()
+ *
+ */
+
 
 /**
  * called from api/createTables (admin only)
  */
-function createTables($conn) {
-    try {
-        pg_query($conn, 'DROP TABLE tblVolAssoc; DROP TABLE tblVolunteers; DROP TABLE tblAssociations;');
-        echo 'Tables dropped. \n';
-    } catch (Exception $e) {
-        echo 'Failed to drop tables: ' . $e->getMessage() . '\n';
-    }
+function createTables($conn)
+{
+    dropTableVolAssoc($conn);
+    dropTableVolunteers($conn);
+    dropTableAssociations($conn);
 
+    createTableVolunteers($conn);
+    createTableAssociations($conn);
+    createTableVolAssoc($conn);
+}
+
+function dropTableVolAssoc($conn)
+{
+    try {
+        pg_query($conn, 'DROP TABLE tblVolAssoc;');
+        echo 'Table tblVolAssoc dropped.<br>';
+    } catch (Exception $e) {
+        echo 'Failed to drop table tblVolAssoc: ' . $e->getMessage() . '<br>';
+    }
+}
+
+function createTableVolAssoc($conn)
+{
+    // asociere intre asociatii si voluntari
+    if (pg_query($conn, 'CREATE TABLE tblVolAssoc(
+    vol_id INTEGER NOT NULL,
+    assoc_id INTEGER NOT NULL,
+    created_on TIMESTAMP NOT NULL,
+    PRIMARY KEY (vol_id, assoc_id),
+    CONSTRAINT volassoc_vol_fkey FOREIGN KEY (vol_id)
+    REFERENCES tblVolunteers(id),
+    CONSTRAINT volassoc_assoc_fkey FOREIGN KEY (assoc_id)
+    REFERENCES tblAssociations(id)
+    )  ')) {
+        echo "Table VolAssoc created!<br>";
+    } else {
+        echo "Table VolAssoc failed! :" . pg_last_error($conn) . "<br>";
+    }
+}
+
+function dropTableVolunteers($conn)
+{
+    try {
+        pg_query($conn, 'DROP TABLE tblVolunteers;');
+        echo 'Table tblVolunteers dropped.<br>';
+    } catch (Exception $e) {
+        echo 'Failed to drop table tblVolunteers: ' . $e->getMessage() . '<br>';
+    }
+}
+
+function createTableVolunteers($conn)
+{
     if (pg_query($conn, 'CREATE TABLE tblVolunteers(
         id serial PRIMARY KEY,
         nume VARCHAR(50) NOT NULL,
@@ -20,9 +82,24 @@ function createTables($conn) {
         updated_on TIMESTAMP NOT NULL,
         last_login TIMESTAMP
         )  ')) {
-        echo "Table Volunteers created!\n";
+        echo "Table Volunteers created!<br>";
+    } else {
+        echo "Table Volunteers failed! :" . pg_last_error($conn) . "<br>";
     }
+}
 
+function dropTableAssociations($conn)
+{
+    try {
+        pg_query($conn, 'DROP TABLE tblAssociations;');
+        echo 'Table tblAssociations dropped.<br>';
+    } catch (Exception $e) {
+        echo 'Failed to drop table tblAssociations: ' . $e->getMessage() . '<br>';
+    }
+}
+
+function createTableAssociations($conn)
+{
     if (pg_query($conn, 'CREATE TABLE tblAssociations(
         id serial PRIMARY KEY,
         nume VARCHAR(50) NOT NULL,
@@ -32,53 +109,122 @@ function createTables($conn) {
         updated_on TIMESTAMP NOT NULL,
         last_login TIMESTAMP
         )  ')) {
-        echo "Table Associations created!\n";
+        echo "Table Associations created!<br>";
+    } else {
+        echo "Table tblAssociations failed! :" . pg_last_error($conn) . "<br>";
     }
-
-    // asociere intre asociatii si voluntari
-    if (pg_query($conn, 'CREATE TABLE tblVolAssoc(
-        vol_id INTEGER NOT NULL,
-        assoc_id INTEGER NOT NULL,
-        created_on TIMESTAMP NOT NULL,
-        PRIMARY KEY (vol_id, assoc_id),
-        CONSTRAINT volassoc_vol_fkey FOREIGN KEY (vol_id)
-        REFERENCES tblVolunteers(id),
-        CONSTRAINT volassoc_assoc_fkey FOREIGN KEY (assoc_id)
-        REFERENCES tblAssociations(id)
-        )  ')) {
-        echo "Table VolAssoc created!\n";
-    }
-
-    insertData($conn);
-
 }
 
-function insertData($conn) {
+/**
+ * insert random data
+ */
+function insertDataAssociations($conn)
+{
+    echo "<b>Warnings are due to unique constraint on email, so it's fine!</b>      <br><br>";
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_URL, "https://randomuser.me/api/");
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
     
+    $orgNameSuffix = array("Charity", "Volunteers", "Organization", "for the People", "United");
 
     for ($xi = 0; $xi < 12; $xi++) {
         $response = curl_exec($curl);
         $err = curl_error($curl);
         $data = json_decode($response, true);
 
-        insert_Assoc($conn,  $data['results'][0]['location']['city'] . " Charity", 
-            $data['results'][0]['location']['street']['name'], $data['results'][0]['email']);
+        insert_Assoc(
+            $conn,
+            $data['results'][0]['location']['city'] . " " . $orgNameSuffix[array_rand($orgNameSuffix)],
+            implode($data['results'][0]['location']['street'], " ") . ", " . $data['results'][0]['location']['state'],
+            $data['results'][0]['email']
+        );
     }
+    
     curl_close($curl);
+}
+
+function insertDataVolunteers($conn)
+{
+    echo "<b>Warnings are due to unique constraint on email, so it's fine!</b>      <br><br>";
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, "https://randomuser.me/api/");
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    
+    $orgNameSuffix = array("Charity", "Volunteers", "Organization", "for the People", "United");
+
+    for ($xi = 0; $xi < 10; $xi++) {
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        $data = json_decode($response, true);
+
+        insert_Volunteers(
+            $conn,
+            $data['results'][0]['name']['last'],
+            $data['results'][0]['name']['first'],
+            $data['results'][0]['email']
+        );
+    }
+
+    
+    curl_close($curl);
+}
+
+/**
+ * Insert random data
+ */
+function insertDataVolAssoc($conn)
+{
+    for ($xi = 1; $xi <= 12; $xi++) { // associatii
+        for ($yi = 1; $yi <= 20; $yi++) { // voluntari
+            insert_VolAssoc($conn, $yi, $xi);
+        }
+    }
 }
 
 /**
  * Insert test data in tblAssociations
  */
-function insert_Assoc($conn, $nume, $adresa, $email) {
+function insert_Assoc($conn, $nume, $adresa, $email)
+{
     $query  ='INSERT INTO tblAssociations 
     (nume, adresa, email, created_on, updated_on) VALUES 
     (\'' . $nume . '\',\'' . $adresa . '\',\'' . $email . '\', current_timestamp, current_timestamp)';
 
-    echo $query . '\n';
+    echo $query . '<br>';
 
-    if (pg_query($conn, $query)) { echo "Record added in tblAssociations";}
+    if (pg_query($conn, $query)) {
+        echo "Record added in tblAssociations";
+    }
+}
+
+/**
+ * Insert test data in tblVolAssoc (with low probability)
+ */
+function insert_VolAssoc($conn, $assoc, $vol)
+{
+    if (rand(0, 100) < 9) {
+        $query  ='INSERT INTO tblVolAssoc 
+        (assoc_id, vol_id, created_on) VALUES 
+        (' . $assoc . ',' . $vol .', current_timestamp)';
+
+        if (pg_query($conn, $query)) {
+            echo "Record added in tblVolAssoc";
+        }
+    }
+}
+
+/**
+ * Insert test data in tblVolunteers
+ */
+function insert_Volunteers($conn, $nume, $prenume, $email)
+{
+    $query  ='INSERT INTO tblVolunteers 
+    (nume, prenume, email, created_on, updated_on) VALUES 
+    (\'' . $nume . '\',\'' . $prenume . '\',\'' . $email . '\', current_timestamp, current_timestamp)';
+
+    echo $query . '<br>';
+
+    if (pg_query($conn, $query)) {
+        echo "Record added in tblVolunteers";
+    }
 }
